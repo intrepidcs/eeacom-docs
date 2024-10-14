5f9ac365d4a42a6d7198cc1b72b84eb527b4bcb7
Praavin Bawwane <pbawane@freewili.com>
2024-10-14 11:51:30 +0530

    favicon and social card image renamed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/eeacom-social-card.jpg',
       navbar: {
         title: 'EEA COM Documentation',
         logo: {
